a2836d9ff1d62eebdf5673d88e8a217f92d4138f
MihirAmin001 <developer@thinfo.in>
2025-09-24 12:58:57 +0530

    Fix Vercel SPA routing configuration to resolve MIME type error

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './router'
 
 function App() {
-  console.log('App component is rendering');
-  
   return (
-    <div>
-      <h1>IT1Stop Application</h1>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
